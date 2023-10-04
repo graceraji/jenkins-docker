f3c20e62f48b77e978f441c5e0b94d80c617e31b
graceraji <144625483+graceraji@users.noreply.github.com>
2023-10-04 16:27:33 -0400

    Create Webapp.java

---
+++ src/main/java/com/javaexpress/docker/webapp/Webapp.java
@@ -0,0 +1,14 @@
+
+package com.javaexpress.docker.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp.class, args);
+	}
+
+}
